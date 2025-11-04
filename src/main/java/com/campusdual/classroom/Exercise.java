df3d77ac7548551f5706affe3ae1eb3ce5e1bcbf
Inés <ines.ramos@campusdual.com>
2025-11-04 17:18:39 +0100

    solved exercise 29

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -2,11 +2,20 @@ package com.campusdual.classroom;
 
 public class Exercise {
 
-    public static int divisionWithCustomException(int dividend, int divisor){
+    public static int divisionWithCustomException(int dividend, int divisor) throws DivisionByZeroException {
+        if (divisor == 0) {
+            throw new DivisionByZeroException("Cannot divide by zero");
+        }return dividend / divisor;
 
     }
 
     public static void main(String[] args) {
+        try {
+            int result = divisionWithCustomException(3, 0);
+            System.out.println("Result: " + result);
+        } catch (DivisionByZeroException e) {
+            System.out.println("Custom exception caught: " + e.getMessage());
+        }
 
     }
 }
